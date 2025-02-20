b065f3ed18b8041fc9a299a8d5f533a210af0208
Vishal-chaudhari12 <126507882+7775080323@users.noreply.github.com>
2025-02-20 16:33:19 +0530

    test commit

--- src/index.ts
+++ src/index.ts
@@ -38,10 +38,6 @@ mongoose
 
 // API Routes
 
-app.get("/", (req, res) => {
-  res.send("Backend is running!");
-});
-
 app.use("/api/user", userRoutes);
 
 // Fetch previous messages from MongoDB
@@ -104,7 +100,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
